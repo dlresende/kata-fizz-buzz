76705980d23abe2a45f244ba9dd140c22e7bd8a5
Diego Lemos <diegolemosderesende@gmail.com>
2017-03-03 18:05:25 +0000

    Add failing test for Fizz

--- main_test.go
+++ main_test.go
@@ -6,6 +6,17 @@ func TestThatGivenANumberNItShouldPrintNumbersFrom1ToN(t *testing.T) {
 	actual := FizzBuzz(2)
 	expected := "1\n2\n"
 
+	assertEquals(actual, expected, t)
+}
+
+func TestThatGivenANumberDivisibleBy3ItShouldPrintFizz(t *testing.T) {
+	actual := FizzBuzz(3)
+	expected := "1\n2\nFizz"
+
+	assertEquals(actual, expected, t)
+}
+
+func assertEquals(actual string, expected string, t *testing.T) {
 	if actual != expected {
 		t.Errorf("\nExpecting\n" + expected + "\nbut got\n" + actual)
 	}
