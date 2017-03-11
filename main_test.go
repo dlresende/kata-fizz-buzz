3e8ee32bc7ecf8d99fc0e348bb19af11c3b6052a
Diego Lemos <diegolemosderesende@gmail.com>
2017-03-11 15:02:54 +0000

    Using raw string literal

--- main_test.go
+++ main_test.go
@@ -7,13 +7,6 @@ func TestThatGivenANumberItShouldPrintTheNumber(t *testing.T) {
 	assertEquals(Replace(2), "2", t)
 }
 
-func TestThatGivenANumberNItShouldPrintNumbersFrom1ToN(t *testing.T) {
-	actual := IterateUntil(5)
-	expected := "1\n2\nFizz\n4\nBuzz\n"
-
-	assertEquals(actual, expected, t)
-}
-
 func TestThatGivenANumberDivisibleBy3ItShouldPrintFizz(t *testing.T) {
 	actual := Replace(3)
 
@@ -34,4 +27,24 @@ func assertEquals(actual string, expected string, t *testing.T) {
 	if actual != expected {
 		t.Errorf("\nExpecting\n" + expected + "\nbut got\n" + actual)
 	}
+}
+
+func TestThatGivenANumberNItShouldPrintNumbersFrom1ToN(t *testing.T) {
+	expected := `1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+`
+	assertEquals(IterateUntil(15), expected, t)
 }
